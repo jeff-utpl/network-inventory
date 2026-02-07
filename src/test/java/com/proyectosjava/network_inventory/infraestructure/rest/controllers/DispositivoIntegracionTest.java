551b475e9e00d2658e4c1c34992a9c7a13e3f67b
Jefferson <juanca-barca@hotmail.com>
2026-02-06 23:57:04 -0500

    fix: actualizacion de librerias en los tests

--- src/test/java/com/proyectosjava/network_inventory/infraestructure/rest/controllers/DispositivoIntegracionTest.java
+++ src/test/java/com/proyectosjava/network_inventory/infraestructure/rest/controllers/DispositivoIntegracionTest.java
@@ -3,7 +3,6 @@ package com.proyectosjava.network_inventory.infraestructure.rest.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -11,7 +10,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest //Levanta todo el contexto de Spring (el edificio completo)
-@AutoConfigureMockMvc //Nos permite simular llamadas HTTP sin abrir el navegador
 public class DispositivoIntegracionTest {
 
     @Autowired
